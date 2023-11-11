464c6029a988dc436dac8ef1a60e42582d6b94d9
NanderTGA <65074195+NanderTGA@users.noreply.github.com>
2023-11-11 14:37:28 +0100

    chore: enable rich presence by default

--- OpenFK/Properties/Settings.Designer.cs
+++ OpenFK/Properties/Settings.Designer.cs
@@ -37,7 +37,7 @@ namespace OpenFK.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
         public bool RPC {
             get {
                 return ((bool)(this["RPC"]));

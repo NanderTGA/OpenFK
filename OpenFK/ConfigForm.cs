30478346f3f34fb0eb25d25923c50d4d585d5727
NanderTGA <65074195+NanderTGA@users.noreply.github.com>
2024-01-04 14:47:05 +0100

    refactor(ConfigForm): make UpdateTextBoxes() explicitly private

--- OpenFK/ConfigForm.cs
+++ OpenFK/ConfigForm.cs
@@ -77,7 +77,7 @@ namespace OpenFK
             UpdateTextboxes();
         }
 
-        void UpdateTextboxes()
+        private void UpdateTextboxes()
         {
             if (Settings.Default.IsOnline)
             {

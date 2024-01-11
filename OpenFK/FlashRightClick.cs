f5e11778070e998c53df257f92880e28b31a166b
NanderTGA <65074195+NanderTGA@users.noreply.github.com>
2024-01-11 22:39:28 +0100

    refactor: flash right click handler

--- OpenFK/FlashRightClick.cs
+++ OpenFK/FlashRightClick.cs
@@ -1,11 +1,8 @@
 ﻿using AxShockwaveFlashObjects;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using OpenFK.OFK.Common;
 
 namespace OpenFK
 {
@@ -13,15 +10,12 @@ namespace OpenFK
     {
         protected override void WndProc(ref Message m)
         {
-            if(m.Msg == 0x0204) //If it's a right click.
+            if(m.Msg == 0x0204 && !ModifierKeys.HasFlag(Keys.Control)) // If it's a right click and control is not held down. If control is held down, the context menu will show.
             {
-                if(!ModifierKeys.HasFlag(Keys.Control)) //If control is not held down | If it is, the context menu will show
-                {
-                    this.SetVariable("msg", @"<rightclick x=""" + Cursor.Position.X + @""" y=""" + Cursor.Position.Y + @""" />"); //Sends right click command to flash.
-                    Debug.WriteLine("right click");
-                    m.Result = IntPtr.Zero; //Blocks context menu from showing on versions that aren't the fetched OCX... No clue why it doesn't work.
-                    return;
-                }
+                this.SetVariable("msg", $@"<rightclick x=""{Cursor.Position.X}"" y=""{Cursor.Position.Y}"" />"); // Sends right click command to flash.
+                LogManager.LogGeneral("Right click");
+                m.Result = IntPtr.Zero; // Blocks context menu from showing on versions that aren't the fetched OCX... No clue why it doesn't work.
+                return;
             }
             base.WndProc(ref m);
         }
